53d5b8aa135fb31cccac9ba3b19d208f1bad9b74
danny <danny@35g.tw>
2014-05-15 16:18:47 +0800

    Initial Commit

---
+++ ListRunningApplication/DYAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DYAppDelegate.h
+//  ListRunningApplication
+//
+//  Created by danny on 2014/5/15.
+//  Copyright (c) 2014年 danny. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface DYAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ ListRunningApplication/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  ListRunningApplication
+//
+//  Created by danny on 2014/5/15.
+//  Copyright (c) 2014年 danny. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
